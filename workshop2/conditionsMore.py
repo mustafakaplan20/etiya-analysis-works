168dddcc6431280b2c654b684436def6655d2a1e
mustafakaplan20 <mustafa.kaplan@etiya.com>
2023-04-06 14:36:47 +0300

    workshop-2 done&committed

---
+++ workshop2/conditionsMore.py
@@ -0,0 +1,13 @@
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6) 
+
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
+
+if final < 40 or ortalama < 50 or vize==final*2:
+    print("Fail")
+else:
+    print("Passed!")
