aaf3dfc4493f07deadf31d14ce1d2269aeed975d
mustafakaplan20 <mustafa.kaplan@etiya.com>
2023-04-06 09:16:30 +0300

    workshop-1 done&committed

---
+++ workshop1/userInput.py
@@ -0,0 +1,9 @@
+total_people=0;
+people_list=[]
+
+while total_people<11:
+    name_surname=input("Enter Name & Surname: ")
+    people_list.append(name_surname)
+    total_people+=1
+
+print(people_list)
