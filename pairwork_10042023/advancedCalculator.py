a5a52f0222e79909faceebda44563443d07b21f9
mustafakaplan20 <mustafa.kaplan@etiya.com>
2023-04-10 15:11:08 +0300

    calcultor updated

--- pairwork_10042023/advancedCalculator.py
+++ pairwork_10042023/advancedCalculator.py
@@ -7,29 +7,39 @@ def multiplication(firstNum: float, secondNum: float):
 def divide(firstNum: float, secondNum: float):
   if (secondNum != 0):
     print(firstNum/secondNum)
+  elif(firstNum==0 and secondNum==0):
+    print("Please use limit(differential) calculator!")
+  else:
+    print("Divider cannot be 0, please check!")
+def mod(firstNum: float, secondNum: float):
+  if(secondNum!=0):
+    print(firstNum%secondNum)
   else:
     print("Divider cannot be 0, please check!")
 
-def calculator(fNum:float,secNum:float,operator:str):
-  if operator== "+":
+def calculator(fNum:float,secNum:float,operation:str):
+  if operation== "+":
     sum(firstNum=fNum,secondNum=secNum)
-  elif operator == "-":
+  elif operation == "-":
     substract(firstNum=fNum, secondNum=secNum)
-  elif operator == "*":
+  elif operation == "*":
     multiplication(firstNum=fNum, secondNum=secNum)
-  elif operator == "/":
+  elif operation == "/":
     divide(firstNum=fNum,secondNum=secNum)
+  elif operation == "%":
+    mod(fNum, secNum)
   else:
     print("Please choose a valid operator!")
 
 while True:
   print("--------------------------------------------------\n")
   firstNum= float(input("Enter your 1st number: "))
-  operator = input("Choose your operation as ( +,-,*, /) :")
+  operator = input("Choose your operation as ( +,-,*, /, %) :")
   secondNum= float(input("Enter your 2nd number: "))
-  calculator(fNum=firstNum,secNum=secondNum,operator=operator)
-  isContinue=input("Do you want to continue?(y/n)")
-  if(isContinue!="y"):
+  
+  calculator(firstNum,secondNum,operator)
+  isContinue=input("Do you want to continue?(Quit for 'q', Continue for 'Enter'!)")
+  if(isContinue=="q"):
     print("\nOperation terminated!")
     break
   else:
